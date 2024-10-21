a33d43c8202e8952f21a575b5efd6711b3bd70ac
giorgi <ge22360017@eu.edu.ge>
2024-10-21 18:16:50 +0400

    lecture14homework

---
+++ app.js
@@ -0,0 +1,58 @@
+let num = 10;
+let square = num * num;
+if (square > 100) {
+  console.log(square + " მეტია 100-ზე");
+} else if (num == 100) {
+  console.log(square + " ნაკლებია 100ზე");
+} else {
+  console.log("კვადრატი 100ია");
+}
+
+//
+//
+//
+let number = 50;
+if (number == 50) {
+  console.log("რიცხვი 50ია");
+}
+if (number == 100) {
+  console.log("რიცხვი 100ია");
+}
+if (number > 50 && number < 100) {
+  console.log(number + " მეტია 50ზე და ნაკლებია 100ზე");
+} else if (number < 50) {
+  console.log(number + " ნაკლებია 50ზე");
+} else {
+  console.log(number + "მეტია 100ზე");
+}
+
+//
+//
+//
+let number2 = 9;
+if (number2 < 0) {
+  console.log(number2 + " უარყოფითია");
+} else if (number2 == 0) {
+  console.log("რიცხვი" + " 0ია");
+} else {
+  console.log(number2 + " მეტია 0ზე");
+}
+
+//
+//
+//
+let length = 12.5;
+let width = 12.5;
+const perimeter = 2 * (length + width);
+if (perimeter > 50) {
+  console.log(perimeter + " მეტია 50ზე");
+} else if (perimeter < 50) {
+  console.log(perimeter + " ნაკლებია 50ზე");
+} else {
+  console.log("პერიმეტრი" + " 50ია");
+}
+
+//
+//
+//
+
